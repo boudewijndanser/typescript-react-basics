c63a823c71e99dedb9e5b7b844e0e77bf5a6921d
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2018-07-19 20:45:11 +0200

    That was fun!

---
+++ README.md
@@ -0,0 +1,5 @@
+# TypeScript React basics
+> Using TypeScript in the front-end!
+
+### Description
+Some basic practice to get started with TypeScript in React
--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import FirstComponent from './FirstComponent'
+import UserComponent from './UserComponent'
+
 ReactDOM.render(
 <div>
 <h1>Hello, Welcome to the first page</h1>
+<FirstComponent />
+<UserComponent name="Boudewijn" age={39.6} address="Planet Earth" dob={new Date()} />
 </div>,
   document.getElementById("root")
 );
